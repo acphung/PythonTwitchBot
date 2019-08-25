d6c9e3a9509cc04bb363138fa505f35f96d9fda1
Alex <35509662+acphung@users.noreply.github.com>
2019-08-24 21:44:38 -0700

    Bot now handles Ping messages from the twitch irc and sends Pong message back

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ load_dotenv()
 HOST = "irc.chat.twitch.tv"
 PORT = 6667
 
-print("Running Bot...")
+print("Running Bot...", flush=True)
 
 irc = socket.socket()
 irc.connect((HOST, PORT))
@@ -15,11 +15,13 @@ irc.send(("NICK " + os.getenv("TWITCH_NICK") + "\r\n").encode())
 
 while True:
     msg = irc.recv(2048).decode()
-    print(msg)
-    # if msg.find(":>") != -1:
-    #     print("CONNECTED")
-    if msg.find(":>") != -1:
+    if msg.find("PING") != -1:
+        print("Received Ping!\nSending Pong Back!", flush=True)
+        irc.send(("PONG :tmi.twitch.tv\r\n").encode())
         break
+    elif msg.find(":>") != -1:
+        print(msg, flush=True)
+        # break
 
 print()
 print("SUCCESSFULLY CONNECTED TO TWITCH IRC")
