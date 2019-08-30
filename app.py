47aa81bb2391d420bd55d06b99a9cd9ebd10d595
Alex <35509662+acphung@users.noreply.github.com>
2019-08-30 00:49:49 -0700

    Clean up some code and added a DEBUG print

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import socket
 load_dotenv()
 HOST = "irc.chat.twitch.tv"
 PORT = 6667
+DEBUG = True
 
 
 def printFlush(msg=""):
@@ -22,18 +23,42 @@ def recvMsg():
 printFlush("Running Bot...")
 
 irc = socket.socket()
+irc.settimeout(30)
 irc.connect((HOST, PORT))
 sendMsg("PASS " + os.getenv("TWITCH_AUTH"))
 sendMsg("NICK " + os.getenv("TWITCH_NICK"))
 
 while True:
-    msg = recvMsg()
+    try:
+        msg = recvMsg()
+    except socket.timeout:
+        printFlush("ERROR: The Socket Timeout!")
+        msg = ""
+
+    if DEBUG:
+        printFlush("DEBUG: [ " + msg + " ]")
+
     if msg.find("PING") != -1:
-        printFlush("Received Ping!\nSending Pong Back!")
+        printFlush("*** Received Ping! Sending Pong Back!")
         sendMsg("PONG :tmi.twitch.tv")
-        break
+        # break
     elif msg.find(":>") != -1:
         printFlush(msg)
+        printFlush()
+        printFlush("*** SUCCESSFULLY CONNECTED TO TWITCH IRC!")
+        printFlush("*** Attempting to Join Channel...")
+        sendMsg("JOIN #zyfae")
+    elif msg.find(":End of /NAMES list") != -1:
+        # printFlush(msg)
+        sendMsg("PRIVMSG #zyfae :Hello, This is Test Bot!")
+    elif msg.find("PRIVMSG") != -1:
+        printFlush(msg)
+        if msg.find("!shutdown") != -1:
+            sendMsg("PRIVMSG #zyfae :Bot is shutting down!")
+            sendMsg("PART #zyfae")
+    elif msg.find("PART") != -1:
+        printFlush("*** Bot has successfully left the channel!")
+        break
 
-printFlush()
-printFlush("SUCCESSFULLY CONNECTED TO TWITCH IRC!")
+printFlush("*** Bot is now shutting down!")
+irc.close()
