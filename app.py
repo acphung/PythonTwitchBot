e0f6c519000297daf629f2aca34df3fdae48ef7f
Alex <35509662+acphung@users.noreply.github.com>
2019-09-02 00:00:23 -0700

    Added some util functions but w/o testing and some extra prints for logging

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 from dotenv import load_dotenv
 import os
 import socket
+import re
 
 # Getting the environment variable needed to authenticate the bot
 load_dotenv()
 HOST = "irc.chat.twitch.tv"
 PORT = 6667
 DEBUG = True
+CHANNELS = ["zyfae"]
 
 # --- Utility Functions --- #
 
@@ -22,10 +24,65 @@ def sendMsg(msg=""):
 def recvMsg():
     return irc.recv(2048).decode()
 
+
+def getChannel(msg):
+    # Returns the Chat Channel Name else returns None
+    tokens = msg.split(":")[1:]
+    result = re.search(r"#(.*)", tokens[0])
+    if result:
+        print(result.groups())
+        return result.group(1)
+    return None
+
+
+def getCommand(msg):
+    # Returns the name of the twitch irc command else returns None
+    tokens = msg.split(":")[1:]
+    result = re.search(r"[.*]* ([^ |\n]*) [.*]*", tokens[0])
+    if result:
+        print(result.groups())
+        return result.group(1)
+    return None
+
+
+def getUsername(msg):
+    # Returns the username else returns None
+    tokens = msg.split(":")[1:]
+    result = re.search(
+        r"<([^ |\n]*)>!<([^ |\n]*)>@<([^ |\n]*)>.tmi.twitch.tv", tokens[0])
+    if result:
+        print(result.groups())
+        return result.group(1)
+    return None
+
+
+def getMsg(msg):
+    # Returns the message else returns None
+    return msg.split(":")[1:][1]
+
+
+def joinChannels(channels):
+    # Join the twitch chat of each of the channel specified
+    for channel in channels:
+        if channel[0] != "#":
+            channel = "#" + channel
+        cmd = "JOIN " + channel
+        sendMsg(cmd)
+        try:
+            msg = recvMsg()
+        except socket.timeout:
+            printFlush("ERROR: The Socket Timeout!")
+            msg = ""
+
+        if msg == "":
+            if msg.find(":End of /NAMES list") != -1:
+                printFlush("*** SUCCESSFULLY JOINED THE CHANNEL")
+                sendMsg("PRIVMSG " + channel + " :Hello, This is a Test Bot!")
+
+
 # --- Main --- #
 
-
-# Start the bot & create the socket & connect to twitch irc server
+ # Start the bot & create the socket & connect to twitch irc server
 printFlush("Running Bot...")
 irc = socket.socket()
 irc.settimeout(30)
@@ -40,7 +97,7 @@ while True:
         msg = recvMsg()
     except socket.timeout:
         printFlush("ERROR: The Socket Timeout!")
-        msg = ""
+        msg = "listening..."
 
     if DEBUG:
         printFlush("DEBUG: [ " + msg + " ]")
@@ -57,8 +114,8 @@ while True:
         printFlush("*** Attempting to Join Channel...")
         sendMsg("JOIN #zyfae")
     elif msg.find(":End of /NAMES list") != -1:
-        # printFlush(msg)
-        sendMsg("PRIVMSG #zyfae :Hello, This is Test Bot!")
+        printFlush("*** SUCCESSFULLY JOINED THE CHANNEL")
+        sendMsg("PRIVMSG #zyfae :Hello, This is a Test Bot!")
     elif msg.find("PRIVMSG") != -1:
         printFlush(msg)
         if msg.find("!shutdown") != -1:
